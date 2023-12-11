e0953d6b8df18803d6ae783223ab4196b44f6e58
Trembleo <trembleleo@gmail.com>
2023-12-12 02:53:43 +0800

    major updates

--- main.py
+++ main.py
@@ -4,29 +4,29 @@ import argparse
 import getpass
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
+from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.schema.runnable import RunnablePassthrough
 from langchain.schema import StrOutputParser
+from langchain.vectorstores import Chroma
 
 from file_extract import extract_content
 
-
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
-
 
 def main():
     if "OPENAI_API_KEY" not in os.environ:
         os.environ["OPENAI_API_KEY"] = getpass.getpass("Please enter your API key: ")
 
-    parser = argparse.ArgumentParser(
-        description="Process a text file and generate relevant content."
-    )
-    parser.add_argument("file_path", help="Path to the input text file")
-
-    args = parser.parse_args()
-
     # FetchDB
-    retriever = extract_content(args.file_path)
+    if os.path.isdir("/vector_dir") == False:
+        print("No folder found, extracting content from file.")
+        file_path = input("Path to the input text file: ")
+        extract_content(file_path)
+    
+    # Retriever
+    vectorstore = Chroma(persist_directory="/vector_dir", embedding_function=OpenAIEmbeddings())
+    retriever = vectorstore.as_retriever()
 
     # LLMs
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.5)
@@ -44,8 +44,9 @@ def main():
         | StrOutputParser()
     )
 
-    return rag_chain.invoke(input_prompt)
-
+    response = rag_chain.invoke(input_prompt)
+    print("Response:", response)
+    return response
 
 if __name__ == "__main__":
     main()
